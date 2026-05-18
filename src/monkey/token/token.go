45a68346a3e7ae889f17764af5c7cabc37813268
Ignacio <ignadiasg@gmail.com>
2026-05-18 16:47:05 -0300

    new tokens

--- src/monkey/token/token.go
+++ src/monkey/token/token.go
@@ -14,11 +14,19 @@ const (
 	// Identifiers + literals
 	IDENT = "IDENT" // names for vars, func,...
 	INT   = "INT"   // 1343456
-
+	TRUE  = "TRUE"
+	FALSE = "FALSE"
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
-
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	SLASH    = "/"
+	ASTERISK = "*"
+	LT       = "<"
+	GT       = ">"
+	EQ       = "=="
+	NOT_EQ   = "!="
+	BANG     = "!"
 	// Delimiters
 	COMMA     = ","
 	SEMICOLON = ";"
@@ -30,7 +38,15 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	VAR      = "VAR"
+	IF       = "IF"
+	RETURN   = "RETURN"
+	ELSE     = "ELSE"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"var": VAR,
+}
 
 func New(tokenType TokenType, char byte) *Token {
 	return &Token{Type: tokenType, Literal: string(char)}
@@ -38,4 +54,11 @@ func New(tokenType TokenType, char byte) *Token {
 
 func NewTokenWithLiteral(tokenType TokenType, str string) *Token {
 	return &Token{Type: tokenType, Literal: str}
+}
+
+func GenerateTokenForWord(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
 }
